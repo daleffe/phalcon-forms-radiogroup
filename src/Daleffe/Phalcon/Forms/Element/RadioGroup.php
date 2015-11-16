eb92e9ff10792eb91a8d5730672427d9004f7dbc
Guilherme Roberge Daleffe <falecomdaleffe@gmail.com>
2015-11-16 11:03:26 -0200

    Added rewrited render method to provide HTML with radio elements

--- src/Daleffe/Phalcon/Forms/Element/RadioGroup.php
+++ src/Daleffe/Phalcon/Forms/Element/RadioGroup.php
@@ -4,5 +4,33 @@ namespace Daleffe\Phalcon\Forms\Element;
 
 class RadioGroup extends \Phalcon\Forms\Element
 {
-	
+	private $checked = null;
+
+	private $html = "";
+
+	public function render($attributes = [])
+	{
+		if (isset($attributes)) {
+			foreach ($attributes as $key => $value) {
+				$this->setAttribute($key, $value);
+			}
+		} else {
+			$attributes = $this->getAttributes();
+		}
+
+		foreach ($attributes['elements'] as $key => $value) {
+			if ($key == $this->getValue()) {
+				$checked = ' checked';
+			} else {
+				$checked = null;
+			}
+
+			$html .= '<div class="' . $attributes['class'][$key] .'">';
+			$html .= '<input type="radio" id="' . $this->getName() . $key . '" name="' . $this->getName() . '" value="' . $key . '"' . $checked . ' />';
+			$html .= '<label for="' . $this->getName() . $key . '">' . $value . '</label>';
+			$html .= "</div>";
+		}
+
+		return $html;
+	}
 }
