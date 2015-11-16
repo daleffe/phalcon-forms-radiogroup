f1ac5e48a342a108d2e6071e7e3786eda254d400
Guilherme Roberge Daleffe <falecomdaleffe@gmail.com>
2015-11-16 13:36:04 -0200

    Removed unused variables and adjusted code according to PSR-2 (Thanks @kassner)

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# phalcon-forms-radiogroup
-Phalcon form element to group radio buttons
+# Phalcon Forms
+## Radio Group
+Phalcon Form element to group Radio inputs.
+
+### Installation
+Update your composer.json with following options:
+```
+{
+	"repositories": [
+		{
+			"type": "vcs",
+			"url": "https://github.com/daleffe/phalcon-forms-radiogroup"
+		}
+	],
+    "require": {
+		  "daleffe/phalcon-forms-radiogroup": "dev-master",
+    }
+}
+```
+> I will check how to put this package in Packagist.org.
+
+### Usage
+In your forms class use:
+``` php
+use Daleffe\Phalcon\Forms\Element\RadioGroup;
+
+// Status
+$status = new RadioGroup("status", [
+  'elements' => array("0" => "Disabled", "1" => "Enabled",
+  'class' => array("radio radio-danger radio-inline", "radio radio-success radio-inline")
+]);
+
+$status->addValidators(array(
+  new PresenceOf(array(
+		'message' => "Status required"
+	)),
+));
+
+if (is_null($devices)) {
+  $status->setDefault('1');
+} else {
+  $status->setDefault($devices->status);
+}
+
+$status->setLabel("Status");
+
+$this->add($status);
+```
+
+So, in view use:
+```php
+{{ form.render('status') }}
+```
+
+This will make the Phalcon correctly apply the filters, validators and *bind()* method to persist in the database.
+
+### Credits
+Credit should be given to [@edwardhew](https://github.com/edwardhew) who proposed this solution in the [Phalcon forum](https://forum.phalconphp.com/discussion/7471/radio-group).
+
+If you have a better proposal on how to apply this solution, open a [issue](https://github.com/daleffe/phalcon-forms-radiogroup/issues/new).
--- src/Daleffe/Phalcon/Forms/Element/RadioGroup.php
+++ src/Daleffe/Phalcon/Forms/Element/RadioGroup.php
@@ -4,33 +4,28 @@ namespace Daleffe\Phalcon\Forms\Element;
 
 class RadioGroup extends \Phalcon\Forms\Element
 {
-	private $checked = null;
+    public function render($attributes = [])
+    {
+        $checked = null;
+        $html = '';
 
-	private $html = "";
+        if (is_array($attributes)) {
+            foreach ($attributes as $key => $value) {
+                $this->setAttribute($key, $value);
+            }
+        } else {
+            $attributes = $this->getAttributes();
+        }
 
-	public function render($attributes = [])
-	{
-		if (isset($attributes)) {
-			foreach ($attributes as $key => $value) {
-				$this->setAttribute($key, $value);
-			}
-		} else {
-			$attributes = $this->getAttributes();
-		}
+        foreach ($attributes['elements'] as $key => $value) {
+            $checked = ($key == $this->getValue() ? ' checked' : null;
 
-		foreach ($attributes['elements'] as $key => $value) {
-			if ($key == $this->getValue()) {
-				$checked = ' checked';
-			} else {
-				$checked = null;
-			}
+            $html .= '<div class="' . $attributes['class'][$key] . '">';
+            $html .= '<input type="radio" id="' . $this->getName() . $key . '" name="' . $this->getName() . '" value="' . $key . '"' . $checked . ' />';
+            $html .= '<label for="' . $this->getName() . $key . '">' . $value . '</label>';
+            $html .= "</div>";
+        }
 
-			$html .= '<div class="' . $attributes['class'][$key] .'">';
-			$html .= '<input type="radio" id="' . $this->getName() . $key . '" name="' . $this->getName() . '" value="' . $key . '"' . $checked . ' />';
-			$html .= '<label for="' . $this->getName() . $key . '">' . $value . '</label>';
-			$html .= "</div>";
-		}
-
-		return $html;
-	}
+        return $html;
+    }
 }
