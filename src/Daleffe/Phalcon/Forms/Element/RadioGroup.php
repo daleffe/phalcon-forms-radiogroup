b393e1a8e1c42e6232497bd7daac3e4759aed9e7
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2019-08-06 17:42:54 -0300

    Bug fixes
    Allowed to remove attribute definition

--- src/Daleffe/Phalcon/Forms/Element/RadioGroup.php
+++ src/Daleffe/Phalcon/Forms/Element/RadioGroup.php
@@ -20,7 +20,7 @@ class RadioGroup extends \Phalcon\Forms\Element
         foreach ($attributes['elements'] as $key => $value) {
             $checked = ($key == $this->getValue()) ? ' checked' : null;
 
-            $html .= '<div class="' . (is_array($attributes['class']) ? $attributes['class'][$key] : $attributes['class']) . '">';
+            $html .= '<div' . (isset($attributes['class']) ? ' class="' . (is_array($attributes['class']) ? $attributes['class'][$key] : $attributes['class']) . '"' : '') . '>';
             $html .= '<input type="radio" id="' . $this->getName() . $key . '" name="' . $this->getName() . '" value="' . $key . '"' . $checked . ' />';
             $html .= '<label for="' . $this->getName() . $key . '">' . $value . '</label>';
             $html .= "</div>";
